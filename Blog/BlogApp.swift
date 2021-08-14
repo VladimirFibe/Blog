7c7f69dd7e2c4e5894c05fec40907cd206dc4640
Vladimir Fibe <motiw@me.com>
2021-08-14 21:24:21 +0600

    Initial Commit

---
+++ Blog/BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogApp.swift
+//  Blog
+//
+//  Created by Владимир on 14.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blog/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blog
+//
+//  Created by Владимир on 14.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
